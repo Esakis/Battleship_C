1e1a30a830cca684d511567a30b48c460513d4a1
michalorlowski@op.pl <michalorlowski@op.pl>
2021-03-17 21:29:56 +0100

    Line 159-176 to check

--- Model/BoardFactory.cs
+++ Model/BoardFactory.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections;
+using Battleship.Model;
 using Battleship.Enums;
 
 namespace Battleship.Model
 {
-    public  class BoardFactory
+    public class BoardFactory
     {
 
-        
-        public static void RandomPlacement(int size, int numbCarrier, int numbCruiser, int numbBattleship, int numbSubmarine, int numbDestroyer)
+
+        public static void RandomPlacement(int size, int numbCarrier, int numbCruiser, int numbBattleship,
+            int numbSubmarine, int numbDestroyer)
         {
             ArrayList blocketPositions = new ArrayList();
             ArrayList listOfShips = new ArrayList();
-            
+
             foreach (var position in listOfShips)
             {
                 Random horizontalRandom = new Random();
@@ -24,77 +26,84 @@ namespace Battleship.Model
                 int shipLeight = 0;
 
 
-                if (numbCarrier>0)
+                if (numbCarrier > 0)
                 {
                     shipLeight = 1;
                     if (startPositionX + shipLeight > size)
                     {
                         break;
                     }
+
                     numbCarrier--;
                 }
-                else if (numbCruiser>0)
+                else if (numbCruiser > 0)
                 {
                     shipLeight = 2;
                     if (startPositionX + shipLeight > size)
                     {
                         break;
                     }
+
                     numbCruiser--;
                 }
-                else if (numbBattleship>0)
+                else if (numbBattleship > 0)
                 {
-                    shipLeight = 3;                       
+                    shipLeight = 3;
                     if (startPositionX + shipLeight > size)
                     {
                         break;
                     }
+
                     numbBattleship--;
                 }
-                else if (numbSubmarine>0)
+                else if (numbSubmarine > 0)
                 {
                     shipLeight = 4;
                     if (startPositionX + shipLeight > size)
                     {
                         break;
                     }
+
                     numbSubmarine--;
                 }
-                else if (numbDestroyer>0)
+                else if (numbDestroyer > 0)
                 {
-                     shipLeight = 5;
+                    shipLeight = 5;
                     if (startPositionX + shipLeight > size)
                     {
                         break;
                     }
+
                     numbDestroyer--;
                 }
+
                 int positionI = startPositionX;
                 int positionJ = startPositionY;
-                    
-                int[,] newShip = new int [1,0];
-                int[,] newBlockedPlace1 = new int [1,0];
-                int[,] newBlockedPlace2 = new int [1,0];
+
+                int[,] newShip = new int [1, 0];
+                int[,] newBlockedPlace1 = new int [1, 0];
+                int[,] newBlockedPlace2 = new int [1, 0];
                 int n = shipLeight;
                 ArrayList blocketPositionsSmall = new ArrayList();
                 ArrayList listOfShipsSmall = new ArrayList();
-                    
-                while (n>0)
+
+                while (n > 0)
                 {
                     n--;
                     newShip[0, 0] = positionI;
                     newShip[1, 0] = positionJ;
-                    newBlockedPlace1 [0, 0] = positionI;
-                    newBlockedPlace1 [1, 0] = positionJ + 1;
-                    newBlockedPlace2 [0, 0] = positionI;
-                    newBlockedPlace2 [1, 0] = positionJ - 1;
-                    foreach (var blockedElement in blocketPositions) //porównuję czy nowy element jest w tablicy zablokowanych
+                    newBlockedPlace1[0, 0] = positionI;
+                    newBlockedPlace1[1, 0] = positionJ + 1;
+                    newBlockedPlace2[0, 0] = positionI;
+                    newBlockedPlace2[1, 0] = positionJ - 1;
+                    foreach (var blockedElement in blocketPositions
+                    ) //porównuję czy nowy element jest w tablicy zablokowanych
                     {
                         if (newShip == blockedElement)
                         {
                             if (shipLeight == 1)
                             {
-                                numbCarrier++; 
+                                numbCarrier++;
                             }
                             else if (shipLeight == 2)
                             {
@@ -112,10 +121,11 @@ namespace Battleship.Model
                             {
                                 numbDestroyer++;
                             }
+
                             break;
                         }
                     }
-                    
+
                     listOfShipsSmall.Add(newShip);
                     blocketPositionsSmall.Add(newBlockedPlace1);
                     blocketPositionsSmall.Add(newShip);
@@ -128,7 +138,7 @@ namespace Battleship.Model
                     {
                         positionJ++;
                     }
-                    
+
                 }
 
                 foreach (var pos in listOfShipsSmall)
@@ -141,10 +151,11 @@ namespace Battleship.Model
                 {
                     blocketPositions.Add(pos);
                 }
-                
+
 
             }
             //Tworzenie tablicy:
+
             var board = new Board(size);
             for (int i = 0; i < board.Ocean.GetLength(0); i++)
             {
@@ -152,9 +163,9 @@ namespace Battleship.Model
                 {
                     foreach (var position in listOfShips)
                     {
-                        if (position[0]== i && position[1]== j)
+                        if (position[0] == i && position[1] == j)
                         {
-                            Point2D = Board.darkCage;
+                            Point2D[i, j] = Board.darkCage;
                         }
                         else
                         {
@@ -162,14 +173,16 @@ namespace Battleship.Model
                         }
                     }
                 }
-                
+
             }
-            
+
         }
 
+/*
         public static Board ManualPlacement()
         {
             throw new NotImplementedException();
         }
+        */
     }
 }
